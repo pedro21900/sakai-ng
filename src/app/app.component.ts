2a587a1166ecbc698c987b8946a670d1d0d8e696
pedro.lenonn <pedro.lenonn@tre-pa.jus.br>
2024-01-16 17:02:43 -0300

    feat: Configuração do tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from 'primeng/api';
 import {RouterOutlet} from '@angular/router';
+import {AppConfig, LayoutService} from "./@core/layout/service/app.layout.service";
 
 @Component({
     selector: 'app-root',
@@ -9,10 +10,21 @@ import {RouterOutlet} from '@angular/router';
     imports: [RouterOutlet]
 })
 export class AppComponent implements OnInit {
+    _config: AppConfig = {
+        ripple: true,
+        inputStyle: 'outlined',
+        menuMode: 'static',
+        colorScheme: 'light',
+        theme: 'md-light-indigo',
+        scale: 14,
+    };
 
-    constructor(private primengConfig: PrimeNGConfig) { }
+    constructor(private primengConfig: PrimeNGConfig,private layoutService:LayoutService) { }
 
     ngOnInit() {
+        this.layoutService.config.set(this._config)
         this.primengConfig.ripple = true;
+
+
     }
 }
